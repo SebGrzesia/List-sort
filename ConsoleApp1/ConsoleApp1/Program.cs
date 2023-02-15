d9114e82cc72d095c8ac7204a2a37068f100b18f
SebastianG <dicoud@gmail.com>
2023-02-15 16:38:30 +0100

    Done

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter list integer number separated by spacebar");
+            string input = Console.ReadLine();
+            int[] numbers = Array.ConvertAll(input.Split(' '), int.Parse);
+
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+
+            Console.WriteLine(string.Join(" ", numbers));
+
+            Console.ReadKey();
+        }
+    }
+}
